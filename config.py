9a93d38dec26c28072791f8792d2eaeab4f7ac5c
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-05 22:10:16 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "7656369802:AAGdlo88cewouuiviq-eHoRHdxj_Ktji3To")
+STRING1 = getenv("STRING_SESSION", "BQFgymkAucNtcQ6rtfuVaJWrXeucZzbPUDFM-twRW2nbvYYoqgdVkhJ-b1rKtJpA81yBLY64oL0vL_XFwpGgQI3vEvXhCuQd3xjUHKU3rR5aG1vVhkBoq4CcFJtsgwPzSmEWjR53MsUlPDZh8GB0Na4dBWpdUsJUfj5LPI6nMGc46sejHRz_uEoQT1a2SWkUqXi-URMQB8nQu0ZpeWTOjgBhSD9kQR462ejjW6EpSScpewXJgU-Y65U3w36gUvVf9fWcg_xJCPDB-T7slefMgkeZrvlXA3creencjVSmhqyD9ljCjo18Ol3W_pRhBK1Q-CELiP3i9V04A99SrrCXNAbU2H8IUAAAAAG0T2GbAA")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://satyam:satyam@satyam.jpftt.mongodb.net/?retryWrites=true&w=majority&appName=Satyam")
 OWNER_ID = int(getenv("OWNER_ID", "7879180190"))
 SUPPORT_GRP = "TheSachinOp"
 UPDATE_CHNL = "TheSachinOp"
